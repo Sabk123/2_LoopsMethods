802ab87ff145aa06e0d061f9a8795b96bca61507
770856 <770856@pdsb.net>
2024-10-03 20:31:12 -0400

    Create seriesPartOne.java

---
+++ 2.1/seriesPartOne.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Group A Counting");
+        for (int i = 0; i <= 12; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int i = 1; i <= 19; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int i = 12; i <= 22; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int i = 101; i <= 115; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        System.out.println("Group B Counting");
+        for (int i = 12; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int i = 20; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int i = 22; i >= 12; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int i = 115; i >= 101; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+
+    }
+}
