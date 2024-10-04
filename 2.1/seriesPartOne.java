525d470a5953ebccd26826f40d6ddff9d3aa59a5
770856 <770856@pdsb.net>
2024-10-03 20:37:33 -0400

    Update seriesPartOne.java

--- 2.1/seriesPartOne.java
+++ 2.1/seriesPartOne.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Group A Counting");
+        System.out.println("\nGroup Z - Example\n");
+        for (int i = 0; i < 5; i++)
+            System.out.print(i + " ");
+        System.out.println();
+        for (int i = 80; i > 70; i--)
+            System.out.print(i + " ");
+        System.out.println();
+        System.out.println("\nGroup A - Counting\n");
         for (int i = 0; i <= 12; i++) {
             System.out.print(i + " ");
         }
@@ -16,8 +23,7 @@ public class Main {
         for (int i = 101; i <= 115; i++) {
             System.out.print(i + " ");
         }
-        System.out.println(" ");
-        System.out.println("Group B Counting");
+        System.out.println("\nGroup B - Backwards\n");
         for (int i = 12; i >= 0; i--) {
             System.out.print(i + " ");
         }
