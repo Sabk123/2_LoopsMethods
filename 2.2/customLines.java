318b0f86689e8b771e19f36c4cfcf7e70454cda4
770856 <770856@pdsb.net>
2024-10-12 09:54:06 -0400

    Update customLines.java
    changed from string to char

--- 2.2/customLines.java
+++ 2.2/customLines.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What character would you like for line #1");
-        String character1 = scanner.next();
+        char character1 = scanner.next().trim().charAt(0); // Only char is allowed, cuts off the input if there is more than one character
         System.out.println("How many do you want?");
         int amount1 = scanner.nextInt();
         for (int i = 1; i <= amount1; i++)
@@ -12,7 +12,7 @@ public class Main {
         System.out.println(" ");
 
         System.out.println("What character would you like for line #2");
-        String character2 = scanner.next();
+        char character2 = scanner.next().trim().charAt(0);
         System.out.println("How many do you want?");
         int amount2 = scanner.nextInt();
         for (int i = 1; i <= amount2; i++)
@@ -20,7 +20,7 @@ public class Main {
         System.out.println(" ");
 
         System.out.println("What character would you like for line #3");
-        String character3 = scanner.next();
+        char character3 = scanner.next().trim().charAt(0);
         System.out.println("How many do you want?");
         int amount3 = scanner.nextInt();
         for (int i = 1; i <= amount3; i++)
@@ -28,14 +28,15 @@ public class Main {
         System.out.println(" ");
 
         System.out.println("What character would you like for line #4");
-        String character4 = scanner.next();
+        char character4 = scanner.next().trim().charAt(0);
         System.out.println("How many do you want?");
         int amount4 = scanner.nextInt();
         for (int i = 1; i <= amount4; i++)
             System.out.print(character4);
         System.out.println(" ");
+
         System.out.println("What character would you like for line #5");
-        String character5 = scanner.next();
+        char character5 = scanner.next().trim().charAt(0);
         System.out.println("How many do you want?");
         int amount5 = scanner.nextInt();
         for (int i = 1; i <= amount5; i++)
